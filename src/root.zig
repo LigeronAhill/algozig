e4eced9072e9866c3acc304e8ab45f6ccd29e284
Aleksandr Provotorov <provotorov@inbox.ru>
2025-06-26 23:22:26 +0300

    mod imports

--- src/root.zig
+++ src/root.zig
@@ -4,11 +4,13 @@
 const std = @import("std");
 var _ = @import("fibonacci.zig");
 const testing = std.testing;
+const sort = @import("sort/sort.zig");
 
 pub export fn add(a: i32, b: i32) i32 {
     return a + b;
 }
 
 test "basic add functionality" {
+    _ = sort;
     try testing.expect(add(3, 7) == 10);
 }
---
+++ src/sort/sort.zig
@@ -0,0 +1,5 @@
+pub const bubble = @import("bubble_sort.zig").bubble_sort;
+pub const counter = @import("counter_sort.zig").counting_sort;
+pub const insertion = @import("insertion_sort.zig").insertion_sort;
+pub const radix = @import("radix_sort.zig").radixSort;
+pub const selection = @import("selection_sort.zig").selection_sort;
